c4b0e482e8ae09c08ad6b075e797e0b65301e6b9
Robert R <rarDevelopment@users.noreply.github.com>
2025-01-17 16:23:35 -0500

    retrieve map from Fortnite API

---
+++ MapScaleCalculator/FortniteApi.cs
@@ -0,0 +1,23 @@
+﻿using Fortnite_API;
+using Fortnite_API.Objects;
+using Fortnite_API.Objects.V1;
+
+namespace MapScaleCalculator;
+
+public class FortniteApi
+{
+    private readonly FortniteApiClient _client;
+
+    public FortniteApi(FortniteApiClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<MapV1?> GetFortniteMapLocations()
+    {
+        var mapInfo = await _client.V1.Map.GetAsync(GameLanguage.EN);
+        return mapInfo is not { IsSuccess: true }
+            ? null
+            : mapInfo.Data;
+    }
+}
